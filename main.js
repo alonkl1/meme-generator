91a6ba8d93d612254710235f90b6e452531d4466
alonkl <alonkl1@gmail.com>
2026-06-12 17:39:57 +0300

    check canvas renders with text

--- main.js
+++ main.js
@@ -1,6 +1,57 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = 'blue'
+var gImgs = [{id: 1, url: 'https://placehold.co/600x400', keywords: ['funny', 'cat']}]
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
-ctx.fillRect(5, 50, 200, 100)
+
+function renderMeme() {
+    
+    const line = gMeme.lines[gMeme.selectedLineIdx]
+    
+    // Get the current image ID from gMeme
+    const imgId = gMeme.selectedImgId
+
+
+    // Find that image in gImgs array (hint: look for the image where id === imgId)
+    const img = gImgs.find(imgObj => imgObj.id === imgId) // YOUR CODE: find the right image object
+
+    const imgEl = new Image()
+    imgEl.src = img.url
+    imgEl.onload = function () {
+        ctx.drawImage(imgEl, 0, 0, canvas.width, canvas.height)
+        
+        ctx.fillStyle = line.color
+        ctx.font = line.size + 'px Arial'
+        ctx.fillText (line.txt, 50, 50)
+    }
+
+    // Get the current text line from gMeme
+
+}
+
+function onTextInput(txt) {
+  // Update the data
+  setLineTxt(txt)
+  
+  // Redraw
+  renderMeme()
+}
+
+function setLineTxt(txt){
+    gMeme.lines[gMeme.selectedLineIdx].txt=txt
+}
+document.getElementById('text-input').addEventListener('input', (e) => {
+  onTextInput(e.target.value)
+})
